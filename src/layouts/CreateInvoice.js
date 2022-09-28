1bec99ecb11567009c965012ad54f7461e9214dc
tushar kale <kaletushar222@gmail.com>
2022-09-29 01:22:42 +0530

    removed config

--- src/layouts/CreateInvoice.js
+++ src/layouts/CreateInvoice.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Toast, ToastContainer } from 'react-bootstrap';
 import ComponentInvoiceForm from '../components/InvoiceForm';
+import axios from 'axios';
 
 class CreateInvoice extends React.Component {
     constructor(props){
@@ -16,7 +17,34 @@ class CreateInvoice extends React.Component {
         if(invoiceObj.amount < 1){
             this.setState({show: true})
         }
-        console.log("invoice submitted")
+
+        var data = JSON.stringify({
+            "collection": "Invoice",
+            "database": "mitramandal",
+            "dataSource": "Cluster0",
+            "projection": {
+                "_id": 1
+            }
+        });
+            
+        var config = {
+            method: 'post',
+            url: 'https://data.mongodb-api.com/app/data-uljen/endpoint/data/v1/action/findOne',
+            headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Request-Headers': '*',
+            'api-key': 'M8xb3jd1AAdXyCqxCqd3Mu40HTVzgGukxA8xe8XoihVCT9x4B4Xjt1F1xsgUb1oJ',
+            },
+            data: data
+        };
+            
+        axios(config)
+        .then(function (response) {
+            console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
     }
     render() {
         const { show } = this.state
