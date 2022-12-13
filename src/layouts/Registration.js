89f9233067a6e2e8772c2f522ab832f0b95db152
tushar kale <kaletushar222@gmail.com>
2022-12-14 01:15:38 +0530

    Api for group registration icomplete

--- src/layouts/Registration.js
+++ src/layouts/Registration.js
@@ -1,27 +1,50 @@
 import React from 'react';
 import ComponentRegistrationForm from '../components/RegistrationForm';
+import {registerGroup} from '../api/GroupApi';
 
 class CreateInvoice extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             showToast: false,
-            invoiceSubmitted: false,
+            registrationSubmitted: false,
             toastMessage: ''
         }
     }
     setShow = (value) => {
         this.setState({ show: value });
     }
-    submitRegistration = (registrationObj) => {
+    submitGroupRegistration = (registrationObj) => {
         console.log(registrationObj)
+        const that = this
+        registerGroup(registrationObj)
+            .then((response) => {
+                that.setState({
+                    registrationSubmitted: true,
+                    showToast: true,
+                    toastMessage: "Group Registered Successfully"
+                })
+                setTimeout(function(){
+                    that.setState({
+                        invoiceSubmitted: false
+                    })
+                }, 2000);
+                
+            })    
+            .catch((err) => {
+                console.log(err)
+                that.setState({
+                    showToast: true,
+                    toastMessage: "Failed to create invoice"
+                })
+            });
     }
     render() {
         return (
             <div className="custom-container layout-container">
                 {/* Toast */}
                 <div className='registration'>
-                    <ComponentRegistrationForm submitRegistration={this.submitRegistration} />
+                    <ComponentRegistrationForm submitGroupRegistration={this.submitGroupRegistration} />
                 </div>
             </div>
         );
